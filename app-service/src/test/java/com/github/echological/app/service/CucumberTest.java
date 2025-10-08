cce777820547c88c26aec1724b20180da7edf46a
Faisal <faisal.amir@avrist.com>
2025-10-08 14:15:53 +0700

    move cucumber class

--- app-service/src/test/java/com/github/echological/app/service/CucumberTest.java
+++ app-service/src/test/java/com/github/echological/app/service/CucumberTest.java
@@ -1,4 +1,4 @@
-package com.github.echological.app.service.bdd;
+package com.github.echological.app.service;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
